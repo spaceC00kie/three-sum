6d0e8e66b9ed1197ab781ae282376162246cdbc8
spaceC00kie <kirstie317@gmail.com>
2021-05-31 23:41:53 -0400

    pseudocode progress

---
+++ main.py
@@ -0,0 +1,16 @@
+# sort
+# 3 pointers
+# while lp != len arr - 2
+    # sum(3 pointers)
+    # if sum == target
+        # append values to out
+        # move lp ++
+        #
+    # if sum > target:
+        # move rp --
+    # else
+        # move mp ++
+    # move lp ++
+    # move mp ++
+# return out
+
