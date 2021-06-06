8baec945df01e46647efe933f8fe53ba263a9280
spaceC00kie <kirstie317@gmail.com>
2021-06-05 23:58:33 -0400

    pseudocode start over

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
 # sort
-# 3 pointers
-# while lp != len arr - 2
-    # sum(3 pointers)
-    # if sum == target
-        # append values to out
-        # move lp ++
-        #
-    # if sum > target:
-        # move rp --
-    # else
-        # move mp ++
-    # move lp ++
-    # move mp ++
-# return out
+# make out array
+# define 3 pointers
+#
 
