fd834c6af4bb6e45ec248d974e33a74fc0e0ae38
spaceC00kie <kirstie317@gmail.com>
2021-06-06 00:55:17 -0400

    finished pseudocode

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 # sort
 # make out array
 # define 3 pointers
-#
-
+# while the lp is less than the length of the array -2
+    # while the mp is less than the rp
+        # check for target
+        # if the sum is less than target mp ++
+        # if sum is greater than target rp --
+    # lp ++
+    # mp lp + 1
+# return out array
