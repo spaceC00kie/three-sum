98f50b3efe6026afe469fc7c5342ae769ccc2eb0
spaceC00kie <kirstie317@gmail.com>
2021-06-07 22:35:42 -0400

    alg progress

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-# sort
-# make out array
-# define 3 pointers
-# while the lp is less than the length of the array -2
-    # while the mp is less than the rp
-        # check for target
-        # if the sum is less than target mp ++
-        # if sum is greater than target rp --
-    # lp ++
-    # mp lp + 1
-# return out array
+def three_sum(arr, target):
+    # sort
+    arr.sort()
+    # make out array
+    out = []
+    # define 3 pointers
+    lp = 0
+    mp = 1
+    rp = len(arr) - 1
+    # while the lp is less than the length of the array -2
+    while lp < len(arr) - 2:
+        # while the mp is less than the rp
+            # check for target
+            # if the sum is less than target mp ++
+            # if sum is greater than target rp --
+        # lp ++
+        # mp lp + 1
+    # return out array
+    return out
