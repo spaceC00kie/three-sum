67e9816faa9bbce07ae9ed46995fe5ae2378a525
spaceC00kie <kirstie317@gmail.com>
2021-06-10 00:59:43 -0400

    code finished

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def three_sum(arr, target):
     # sort
     arr.sort()
     # make out array
-    out = []
+    out = [[]]
     # define 3 pointers
     lp = 0
     mp = 1
@@ -10,10 +10,20 @@ def three_sum(arr, target):
     # while the lp is less than the length of the array -2
     while lp < len(arr) - 2:
         # while the mp is less than the rp
+        while mp < rp:
             # check for target
+            total = arr[lp] + arr[mp] + arr[rp]
+            if total == target:
+                arr.append()
             # if the sum is less than target mp ++
+            elif total < target:
+                mp += 1
             # if sum is greater than target rp --
+            else: # total > target:
+                rp -= 1
         # lp ++
+        lp += 1
         # mp lp + 1
+        mp = lp + 1
     # return out array
     return out
